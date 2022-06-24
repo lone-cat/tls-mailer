dd22e43f06f99bc8dcd40e1fc37425798debf201
Alex <alex@melentev.net>
2022-06-24 19:06:02 +0300

    upd

--- simpleemail/helpers.go
+++ simpleemail/helpers.go
@@ -6,15 +6,6 @@ func copySlice[T any](src []T) []T {
 	return dst
 }
 
-func copyMap[K comparable, V any](src map[K]V) map[K]V {
-	dst := make(map[K]V)
-	for key, val := range src {
-		dst[key] = val
-	}
-
-	return dst
-}
-
 func copyHeadersMap(src map[string][]string) map[string][]string {
 	dst := make(map[string][]string)
 	for headerName, headerValuesSlice := range src {
